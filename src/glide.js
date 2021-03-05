20a39e239aede82fce9dd41723f9299a02efa850
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-05 14:44:02 -0500

    Create glide file and import

---
+++ src/glide.js
@@ -0,0 +1,18 @@
+import Glide from '@glidejs/glide'
+const config = {
+  type: 'carousel',
+  perView: 3,
+  focusAt: 'center',
+  autoplay: 2500,
+  dragTreshold: 120,
+  breakpoints: {
+    800: {
+      perView: 2
+    },
+
+    500: {
+      perView: 1
+    }
+  }
+}
+new Glide('.glide', config).mount()
