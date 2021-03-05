8b031ea288cbc5d8d01456a2fca660d769bac772
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-05 14:02:15 -0500

    Make customer an extension of repo

--- src/customer.js
+++ src/customer.js
@@ -1,12 +1,32 @@
-class Customer {
+import HotelRepository from './hotel-repository'
+class Customer extends HotelRepository {
   constructor({id, name}) {
+    super();
     this.id = id;
     this.name = name;
     this.totalSpent = 0;
-    this.bookings = {
-      past: [],
-      upcoming: []
-    }
+    this.bookedRooms = [];
+  }
+  getBookings() {
+    this.bookings.forEach(booking => {
+      if (booking.userID === this.id) {
+        this.bookedRooms.push(booking)
+      }
+    })
+  }
+
+  getTotal() {
+    console.log
+    return this.bookedRooms.reduce((totalSpent, booking) => {
+      console.log(booking)
+      const matchingRoom = 
+      this.rooms.find(room => room.number === booking.roomNumber);
+      // console.log(matchingRoom)
+      if (matchingRoom) {
+        totalSpent += matchingRoom.costPerNight;
+      }
+      return totalSpent
+    }, 0)
   }
 }
 
--- test/customer-test.js
+++ test/customer-test.js
@@ -1,12 +1,20 @@
 import chai from 'chai';
 const expect = chai.expect;
+import HotelRepository from '../src/hotel-repository';
+import Booking from '../src/Booking';
+import Room from '../src/Room';
 import Customer from '../src/customer.js';
+import rooms from './Room-test-data';
+import bookings from './Booking-test-data'
 import userData from './cutomer-test-data'
 
 describe('Customer', function() {
   let customer;
+  let hotelRepo;
   beforeEach(() => {
-    customer = new Customer(userData[0]);
+    hotelRepo = new HotelRepository();
+    hotelRepo.customers = userData.map(user => new Customer(user));
+    customer = new Customer(hotelRepo.customers[0]);
   })
   it('should have an id', function() {
     expect(customer.id).to.deep.equal(1);
@@ -14,13 +22,25 @@ describe('Customer', function() {
   it('should have a name', function() {
     expect(customer.name).to.deep.equal("Leatha Ullrich");
   });
-  it('should have default to no past bookings', function() {
-    expect(customer.bookings.past).to.deep.equal([]);
-  });
-  it('should have default to no future bookings', function() {
-    expect(customer.bookings.upcoming).to.deep.equal([]);
+  it('should have default to no bookings', function() {
+    expect(customer.bookedRooms).to.deep.equal([]);
   });
   it('should have default totalSpent of 0', function() {
     expect(customer.totalSpent).to.deep.equal(0);
   });
-});
+  describe('getBookings', () => {
+    it('should populate the customers bookedRooms array', () => {
+      customer.bookings = bookings;
+      customer.getBookings();
+      expect(customer.bookedRooms.length).to.deep.equal(1);
+    })
+  })
+  describe('getTotal', () => {
+    it('should be able to sum a users booked rooms prices', () => {
+      customer.bookings = bookings;
+      customer.rooms = rooms;
+      customer.getBookings();
+      expect(customer.getTotal()).to.deep.equal(172.09);
+    })
+})
+})
