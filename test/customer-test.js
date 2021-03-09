8ed7f5ab9b0a8154338fe76ec0e166e2fbe5169d
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-09 09:18:15 -0500

    Add sad path testing for user methods

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,8 +1,6 @@
 import chai from 'chai';
 const expect = chai.expect;
 import HotelRepository from '../src/hotel-repository';
-import Booking from '../src/Booking';
-import Room from '../src/Room';
 import Customer from '../src/customer.js';
 import rooms from './Room-test-data';
 import bookings from './Booking-test-data'
@@ -43,11 +41,21 @@ describe('Customer', function() {
       customer.getBookings();
       expect(customer.getTotal()).to.deep.equal(172.09);
     })
+    it('should start at 0 if the customer has yet to make any bookings', () => {
+      customer.bookings = [];
+      expect(customer.getTotal()).to.deep.equal(0);
+    })
   })
   describe('filterByDate', () => {
     it('should be able to filter rooms available on a date', () => {
       expect(customer.filterByDate("2020/02/16")).to.have.lengthOf(11);
     })
+    it('should give back an empty array if no rooms are available', () => {
+      customer.bookings = [bookings[2]];
+      customer.rooms = [rooms[11]];
+      
+      expect(customer.filterByDate("2020/01/10")).to.deep.equal([]);
+    }) 
   })
   describe('filterByType', () => {
     it('should be able to filter those available rooms by type', () => {
