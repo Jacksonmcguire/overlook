65c4a4c663a35951fec257576a5dc5df1cf98243
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-09 14:49:50 -0500

    Remove autoplay

--- src/glide.js
+++ src/glide.js
@@ -2,7 +2,6 @@ import Glide from '@glidejs/glide'
 export const config = {
   perView: 3,
   dragTreshold: 0,
-  autoplay: 3000,
   breakpoints: {
 
     760: {
@@ -11,7 +10,6 @@ export const config = {
 
     500: {
       perView: 1.8,
-      autoplay: false
     }
   },
   arrows: true
